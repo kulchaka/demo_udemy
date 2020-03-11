b8df47c615bd65ff5107155603165c2258a2da79
kulchaka <kulchaka@users.noreply.github.com>
2020-03-11 20:42:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log(leftBorder);
+var leftBorder = 1;
+leftBorder = leftBorder + 12;
+console.log(leftBorder);
+
+
+
+let a = 5;
+let b = "Ivan";
+let c = a + b;
+console.log(c);
